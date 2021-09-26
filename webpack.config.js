3dbe3ee50ff7745b888b5ee08fe388997dd1ab73
joris <jorisvddonk@gmail.com>
2021-09-26 21:11:13 +0200

    copy webrender-basic

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './main.mjs',
@@ -9,5 +10,12 @@ module.exports = {
   resolve: { fallback: { fs: false } },
   optimization: {
     minimize: false, // TODO: re-enable again later; at the moment this breaks closebrace/openbrace in Tzo because minimization gets rid of their names, which are required to do brace matching
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "webRender-basic.js", to: "webRender-basic.js" }
+      ],
+    }),
+  ],
 };
